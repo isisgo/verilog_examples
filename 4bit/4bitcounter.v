96e21ea2d0da6efd7f1f45866761527bb63eb2f8
Isis Oliveira <31692595+isisgo@users.noreply.github.com>
2018-08-22 09:32:28 -0300

    update

--- 4bit/4bitcounter.v
+++ 4bit/4bitcounter.v
@@ -1,17 +1,13 @@
 // Contador 4-bit com clear e enable 
-module count4 (clk, clear, enable, out[3:0]);
+module count4 (clk, clear, enable, out);
 	input clk;
 	input clear; 	
 	input enable; 	
-	output [3:0] out; 
-	wire clk;
-	wire clear;
-	wire enable;
-	reg [3:0] out;
+	output reg [3:0] out; 
 
 always @(posedge clk)
 	begin
 	if (clear) out <= 4'b0;
 	else if (enable) out <= out + 4'b1;
+	end
 endmodule
-
